6b0fc54605372100c600badf1608d0d982266555
TheRaccoon161 <44701351+TheRaccoon161@users.noreply.github.com>
2020-06-27 08:25:59 +0500

    Update qmltranslator.h

--- cpp/qmltranslator.h
+++ cpp/qmltranslator.h
@@ -15,11 +15,9 @@ public:
     explicit QmlTranslator(QObject *parent = 0);
 
 signals:
-    // Сигнал об изменении текущего языка для изменения перевода интерфейса
     void languageChanged();
 
 public:
-    // Метод установки перевода, который будет доступен в QML
     Q_INVOKABLE void setTranslation(QString translation);
 
 private:
